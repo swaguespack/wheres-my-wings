76491681b5829966d46a01f4a187aefe08170350
mikafeng <110942241+mikafeng@users.noreply.github.com>
2023-02-04 13:45:11 -0500

    added handlebars configuration

--- server.js
+++ server.js
@@ -1,22 +1,39 @@
 
 // Dependencies
 const express = require('express');
+const exphbs = require('express-handlebars');
 const path = require('path');
+const hbs = exphbs.create({});
 
 // Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.get("/", (req,res) =>{
-    console.log("welcome")
-    res.send("where's my wings??")
-})
 
+// Set Handlebars as the default template engine.
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+
+// app.get("/", (req,res) =>{
+//     console.log("welcome")
+//     res.send("where's my wings??")
+// })
+
+
+// middle ware 
 app.use(express.static(path.join(__dirname, 'public')));
-// // Sets up the routes
-// app.use(require('./controllers/dish-routes'));
+// Sets up the routes
+// app.use(require(''));
+
 
 // Starts the server to begin listening
 app.listen(PORT, () => {
   console.log('Server listening on: http://localhost:' + PORT);
 });
+
+
